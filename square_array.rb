56e3b0c940582a92145a2e790952ac58f16223a9
Erin Walker <erinb0317@hotmail.com>
2018-01-28 23:04:46 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+
+def square_array(numbers)
+  square_array.each do |numbers[]**|
+end
+
+new_numbers = [9,10,16,25]
+
+def square_array(new_numbers)
+  square_array.each do |new_numbers[]**|
 end
