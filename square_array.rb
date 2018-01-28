1f96e826d967ead6e9af91ffdbaea73cd6f622f6
Erin Walker <erinb0317@hotmail.com>
2018-01-28 23:06:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,12 @@ numbers = [1,2,3]
 
 def square_array(numbers)
   square_array.each do |numbers|
+  end
 end
 
 new_numbers = [9,10,16,25]
 
 def square_array(new_numbers)
   square_array.each do |new_numbers|
+  end
 end
